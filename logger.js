2bd0aacb5dcc203f91dbd041aa3c383596773dec
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2015-10-05 10:30:30 +0200

    firebase init on script load

--- logger.js
+++ logger.js
@@ -8,18 +8,23 @@ window.onblur = function() {
     isActive = false;
 };
 // Include Firebase library
-var firebase_script = document.createElement('script'); firebase_script.src = 'https://cdn.firebase.com/js/client/2.2.9/firebase.js'; document.head.appendChild(firebase_script);
+var firebase_script = document.createElement('script');
+firebase_script.src = 'https://cdn.firebase.com/js/client/2.2.9/firebase.js';
+document.getElementsByTagName('head')[0].appendChild(firebase_script);
 
-var firebase_reference = new Firebase("https://crowdworker-logger.firebaseio.com/trials");
-// Log event to Firebase bucket
-firebase_reference.push({
-    pathname: document.location.pathname,
-    search: document.location.search,
-    hostname: document.location.hostname,
-    hash: document.location.hash
-});
+// when Firebase library is loaded - initialize and use it
+firebase_script.onload = function() {
+    var firebase_reference = new Firebase("https://crowdworker-logger.firebaseio.com/trials");
+    // Log event to Firebase bucket
+    firebase_reference.push({
+        pathname: document.location.pathname,
+        search: document.location.search,
+        hostname: document.location.hostname,
+        hash: document.location.hash
+    });
 
-// test
-setInterval(function() {
-    console.log(window.isActive ? 'active' : 'inactive');
-}, 1000);
+    // test
+    setInterval(function() {
+        console.log(window.isActive ? 'active' : 'inactive');
+    }, 1000);
+};
