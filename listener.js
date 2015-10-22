35e0e3b8de7952239622843122b2d669b7181384
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2015-10-22 17:20:24 +0200

    listener

--- listener.js
+++ listener.js
@@ -1,9 +1,9 @@
 var Firebase = require('firebase');
 var Ref = new Firebase('https://crowdworker-logger.firebaseio.com/'+"localhost"+'/');
 Ref.on('child_changed', function(task_snapshot, prevchildname) {
-	var task_reference = Ref.child(task_snapshot.key()+'/logs');
-	task_reference.on('child_changed', function(assignment_snapshot, prevchildname){
+	console.log(task_snapshot.key());
+	var task_reference = Ref.child(task_snapshot.key());
+	task_reference.on('child_added', function(assignment_snapshot, prevchildname){
 		console.log(assignment_snapshot.key());
 	});
-	
 });
